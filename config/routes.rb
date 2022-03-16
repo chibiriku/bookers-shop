7fe2b848587a9197fb2692a45f07ac7de1100fed
chibiriku <96232950+chibiriku@users.noreply.github.com>
2022-03-16 01:02:00 +0000

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
+  resources :addresses, only:[:index,:edit,:update,:destroy,:create]
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
